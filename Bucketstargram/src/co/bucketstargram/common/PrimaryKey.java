42373aa6a96c5dd69fe586bf9e50f4220f5a962d
이재문 <@LAPTOP-TO8076OJ>
2019-08-20 13:30:47 +0900

    TEST

---
+++ Bucketstargram/src/co/bucketstargram/common/PrimaryKey.java
@@ -0,0 +1,14 @@
+package co.bucketstargram.common;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class PrimaryKey {
+	public static String create() {
+		Date now = new Date(); 
+		String PrimaryKey = new SimpleDateFormat("yyyyMMddHHmmssSSS", Locale.ENGLISH).format(now);
+		
+		return PrimaryKey;
+	}
+}
